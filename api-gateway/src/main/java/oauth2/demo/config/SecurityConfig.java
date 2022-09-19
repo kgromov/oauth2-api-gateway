3ac2a254ab1300916b4054bc5121242c5b1368fe
Konstantin <konstantin@revimedia.com>
2022-09-19 23:52:39 +0300

    Example how vault can be used

--- api-gateway/src/main/java/oauth2/demo/config/SecurityConfig.java
+++ api-gateway/src/main/java/oauth2/demo/config/SecurityConfig.java
@@ -1,16 +1,30 @@
 package oauth2.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import javax.annotation.PostConstruct;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Value("${client-id}")
+    private String clientId;
+
+    @Value("${client-secret}")
+    private String clientSecret;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("clientId = " + clientId);
+        System.out.println("clientSecret = " + clientSecret);
+    }
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
